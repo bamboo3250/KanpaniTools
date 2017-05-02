695cfd0bc278d237b842eb3118fb5f69489c06b2
Truong Ngoc Khanh <bambooVN_3250@yahoo.com>
2017-05-02 12:34:00 +0800

    add tracking popup

--- popup.js
+++ popup.js
@@ -7,22 +7,27 @@
         $("#extension-version").text(version);
 
         $('#play_dmm').click(function() {
+            _gaq.push(['_trackEvent', "Play DMM clicked", 'clicked']);
             window.open('http://www.dmm.com/netgame/social/-/gadgets/=/app_id=181259/');
         });
 
         $('#wiki').click(function() {
+            _gaq.push(['_trackEvent', "Wiki clicked", 'clicked']);
             window.open('http://wikiwiki.jp/kanpani/');
         });
 
         $('#hbc-wiki').click(function() {
+            _gaq.push(['_trackEvent', "HBC Wiki clicked", 'clicked']);
             window.open('http://harem-battle.club/wiki/Kanpani-Girls');
         });
 
         $('#twitter').click(function() {
+            _gaq.push(['_trackEvent', "Twitter clicked", 'clicked']);
             window.open('https://twitter.com/kanpani_STAFF');
         });
 
         $('#discord').click(function() {
+            _gaq.push(['_trackEvent', "Discord clicked", 'clicked']);
             window.open('https://discord.gg/CBpuFxV');
         });
     });
